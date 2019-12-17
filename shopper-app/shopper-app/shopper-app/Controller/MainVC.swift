9c81d8f7415cde7c21d5e73954cf8df2c2db33d5
Jukess <juancaluque@hotmail.es>
2019-12-17 08:47:34 -0600

    lesson-01 done

--- shopper-app/shopper-app/shopper-app/Controller/MainVC.swift
+++ shopper-app/shopper-app/shopper-app/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  shopper-app
 //
 //  Created by Juan Luque on 12/17/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ shopper-app/shopper-app/shopper-app/Views/CurrencyTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyTextField.swift
+//  shopper-app
+//
+//  Created by Juan Luque on 12/17/19.
+//  Copyright © 2019 Juan Luque. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+    override func awakeFromNib() {
+        super .awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9954431653, green: 0.9883316159, blue: 1, alpha: 0.2495048415)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        if let p = placeholder{
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white] )
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+}
